7f4d8609a7a68e95e48e7fbd4acfb5e604167c89
matib83 <efbingenieria@gmail.com>
2022-08-18 11:27:11 -0300

    Creamos modelo y controlador para sacarlo de index.

--- index.js
+++ index.js
@@ -3,49 +3,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('./mongo')
-const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const notesRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
 app.get('/', (request, response) => {
   response.send('<h1>API Blogs</h1>')
 })
+
+app.use('/api/blogs', notesRouter)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ models/Blog.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose')
+
+const blogSchema = new Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Blog = model('Blog', blogSchema)
+module.exports = Blog
