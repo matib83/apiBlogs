44a2adf64684e392fd1cb7f8c3c3db3c9cb4a66c
matib83 <efbingenieria@gmail.com>
2022-08-19 09:08:51 -0300

    Armo un testing con describe completo

---
+++ test/list_helper.test.js
@@ -0,0 +1,26 @@
+const listHelper = require('../utils/list_helper')
+const { listEmptyBlog, listWithOneBlog, listBlogs } = require('./helpers')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(listEmptyBlog)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listBlogs)
+    expect(result).toBe(36)
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs === (null || undefined)) return 0
+  let sumLikes = 0
+  blogs.forEach(blog => { sumLikes += blog.likes })
+  return sumLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
