d39a7ee30a0e5569d57705e71a483b6304a93735
matib83 <efbingenieria@gmail.com>
2022-08-24 11:43:31 -0300

    Manejo de 2 bases de datos, test y desarrollo

--- mongo.js
+++ mongo.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
-const { MONGO_DB_URI } = process.env
 
-mongoose.connect(MONGO_DB_URI, {
+// Extrigo todas las variables de entorno que necesitamos
+const { MONGO_DB_URI, MONGO_DB_URI_TEST, NODE_ENV } = process.env
+
+const connectionString = NODE_ENV === 'test' // cuando la variable de entorno node_env es test, utilizo la BD de test
+  ? MONGO_DB_URI_TEST
+  : MONGO_DB_URI
+
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
