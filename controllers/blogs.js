cc57f600798a6c4613a932ba57274b5630c7dcc5
matib83 <efbingenieria@gmail.com>
2022-08-26 11:58:21 -0300

    Ej 4.11 Hago que los likes por defecto cuando no se pasan en POST, valgan 0.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -8,6 +8,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const newBlog = new Blog(request.body)
+  // Extraigo el campo likes del post del blog
+  if (newBlog.likes === undefined) {
+    newBlog.likes = 0
+  }
+  console.log({ newBlog })
+  // Almaceno los datos del blog en la base de datos
   const saveBlog = await newBlog.save()
   response.status(201).json(saveBlog)
 })
